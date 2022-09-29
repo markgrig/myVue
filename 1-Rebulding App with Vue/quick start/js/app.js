4ce6f5f2cd02fcc53c63e919a9d566763422c1b6
markgrig <110450201+markgrig@users.noreply.github.com>
2022-09-29 15:10:50 +0300

    first

---
+++ 1-Rebulding App with Vue/quick start/js/app.js
@@ -0,0 +1,22 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            userInput: "",
+            allUserInput:  JSON.parse(localStorage.getItem( "allUserInput") ) || [],
+        };
+    },
+    methods: {
+        saveElement(){
+            if (this.userInput !== "") {
+                console.log(this.allUserInput);
+            this.allUserInput.push(this.userInput);
+            localStorage.setItem( "allUserInput", JSON.stringify(this.allUserInput))
+            }
+        },
+        clearLocalStorage() {
+            localStorage.clear();
+            this.allUserInput = [];
+        }
+    },
+}).mount("#app");
+
