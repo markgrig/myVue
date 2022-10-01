3c2aa5e88cf9849c539e603d57fe16dec36c73a7
markgrig <110450201+markgrig@users.noreply.github.com>
2022-10-01 21:23:50 +0300

    01/10/2022

---
+++ 1-Rebulding App with Vue/list of expenses/js/app.js
@@ -0,0 +1,49 @@
+
+const app = Vue.createApp({
+    data() {
+        return {
+            userInput: "",
+            allUserInput:  JSON.parse(localStorage.getItem( "allUserInput") ) || [],
+            sumEpenses:  Number(JSON.parse(localStorage.getItem( "sumEpenses") )) || 0,
+            placeholderInput: "",
+            validation: false,
+        };
+    },
+    methods: {
+        saveElement(){
+            if( this.validation ) {
+                this.allUserInput.push(this.userInput);
+                this.sumEpenses = 0;
+                this.allUserInput.forEach(element => {
+                    this.sumEpenses += Number(element);
+                });
+                this.userInput = ""
+                localStorage.setItem( "sumEpenses", JSON.stringify(this.sumEpenses))
+                localStorage.setItem( "allUserInput", JSON.stringify(this.allUserInput))
+            }
+
+        },
+        clearLocalStorage() {
+            localStorage.clear();
+            this.allUserInput = [];
+            this.sumEpenses = 0;
+        },
+        validationFun() {
+            if ( Number(this.userInput) ) {
+                this.validation = true;
+                this.placeholderInput =  `<div class= "placeholder-input-sucsess"> Успешно!   </div>`
+            }
+            else {
+                this.placeholderInput = `<div class= "placeholder-input-error"> Это не величина расхода!   </div>`
+                this.validation = false;
+                if ( this.userInput === "") {
+                    this.placeholderInput = "";
+                }
+            }
+
+        }
+    },
+})
+
+app.mount("#app");  
+
--- 1-Rebulding App with Vue/shopping list/src/js/app.js
+++ 1-Rebulding App with Vue/shopping list/src/js/app.js
@@ -4,15 +4,54 @@ const app = Vue.createApp({
     data() {
         return {
             formHTML: '',
+            backgroundColor: { background: "rgba(255, 255, 255, 0.95)", text: "black"},
+            topicColor: { text : "rgba(255, 255, 255, 1)" , 
+                        background: "linear-gradient(to top, rgba(90, 63, 230, 0.7) 10%,rgba(80, 80, 234, 0.3) 100%);", },
+            tableColor: "linear-gradient(to top, rgba(90, 63, 230, 0.7) 10%,rgba(80, 80, 234, 0.3) 100%);",
+            backgroundColorArray: [
+                { background: "rgba(27, 51, 148, 0.365)",  text : "white" },
+                { background: "rgba(153, 28, 187, 0.2)",  text : "black" },
+                { background: "rgba(38, 240, 203, 0.349)",  text : "white" },
+                { background: "rgba(255, 255, 255, 0.95)",  text : "black" },
+            ],
+            topicColorArray: [
+                { background: "rgba(255, 255, 255, 0.95)" , text : "rgb(101, 78, 212)" },
+                { background: "rgba(255, 255, 255, 0.95)" , text : "rgb(75, 164, 247)" },
+                { background: "rgba(17, 234, 250, 0.767)" , text : "rgba(255, 255, 255, 1)"  },
+                { background: "rgba(17, 234, 250, 0.767)" , text : "rgba(255, 255, 255, 1)" },,
+            ]
         };
-
     },
     methods: {
         openForm() {
            this.formHTML = `
            <div class="modal-window">
-                <div class="pannel-modal-window center-block"> </div>  
+                <div class="pannel-modal-window center-block"  v-if = "topicColor" v-text= "topicColor"> 
+                </div>  
            </div>`
+        },
+        changeBackgroundColor() {
+            //Math.floor (Math.random () * (max - min + 1)) + min
+            let randNumber = Math.floor (Math.random () * (4)) + 1;
+            let newBackgroundColor = this.backgroundColorArray[randNumber-1];
+            
+            while( this.backgroundColor === newBackgroundColor) {
+                randNumber = Math.floor (Math.random () * (4)) + 1;
+                newBackgroundColor = this.backgroundColorArray[randNumber-1]  
+            }
+
+            randNumber = Math.floor (Math.random () * (4)) + 1;
+            let newtopicColor = this.topicColorArray[randNumber-1];
+
+            while( this.topicColor === newtopicColor) {
+                randNumber = Math.floor (Math.random () * (4)) + 1;
+                newtopicColor = this.topicColorArray[randNumber-1];
+            }
+
+            console.log(newtopicColor, newBackgroundColor);
+           
+            this.topicColor = newtopicColor;
+            this.backgroundColor = newBackgroundColor;
         }
     }
 })
