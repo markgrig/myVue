fb30ae77e46757ff1bb2cc446cb185e26a2b186e
markgrig <110450201+markgrig@users.noreply.github.com>
2022-10-02 21:00:16 +0300

    02/10/2022

--- 1-Rebulding App with Vue/list of expenses/js/app.js
+++ 1-Rebulding App with Vue/list of expenses/js/app.js
@@ -6,6 +6,7 @@ const app = Vue.createApp({
             allUserInput:  JSON.parse(localStorage.getItem( "allUserInput") ) || [],
             sumEpenses:  Number(JSON.parse(localStorage.getItem( "sumEpenses") )) || 0,
             placeholderInput: "",
+            placeholderClearInput: "",
             validation: false,
         };
     },
@@ -20,26 +21,34 @@ const app = Vue.createApp({
                         this.sumEpenses += Number(element);
                     });
                     this.userInput = "";
+                    this.placeholderInput =  ``;
                     localStorage.setItem( "sumEpenses", JSON.stringify(this.sumEpenses))
                     localStorage.setItem( "allUserInput", JSON.stringify(this.allUserInput))
                 }
             }
-
+        
         },
         clearLocalStorage() {
             localStorage.clear();
             this.allUserInput = [];
             this.sumEpenses = 0;
         },
+        clearInput() {
+            this.userInput = "";
+            this.placeholderInput =  ``;
+        },
         validationFun(event) {
             this.userInput = event.target.value;
-            
+
             if ( Number(this.userInput) ) {
                 this.validation = true;
-                this.placeholderInput =  `<div class= "placeholder-input-sucsess"> Успешно!   </div>`
+                this.placeholderInput =  `<div class= "placeholder-input-sucsess"> Число!   </div>`;
+                this.placeholderClearInput = ``
+                
             }
             else {
-                this.placeholderInput = `<div class= "placeholder-input-error"> Это не величина расхода!   </div>`
+                this.placeholderInput = `<div class= "placeholder-input-error"> Не число!   </div>`
+                this.placeholderClearInput = `<div class="clear-input"> 🧺 </div> ` 
                 this.validation = false;
                 if ( this.userInput === "") {
                     this.placeholderInput = "";
