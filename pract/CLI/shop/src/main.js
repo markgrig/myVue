2687bd6dd992af3cd35c35952fe4ba6f7d3a07ea
markgrig <mark3012018@mail.ru>
2022-10-23 05:06:22 +0700

    23/10/22

--- pract/CLI/shop/src/main.js
+++ pract/CLI/shop/src/main.js
@@ -26,7 +26,7 @@ const createchangerDocumentTitle = (router) => {
 createIcons("https://cdn-icons-png.flaticon.com/512/478/478045.png")
 createchangerDocumentTitle(router)
 
-const url = "/.netlify/functions/submission-created"
+const url = "https://markshop-fb0bd-default-rtdb.firebaseio.com/"
 const res = fetch(url, {
     method: 'GET'
 })
