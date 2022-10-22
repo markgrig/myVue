5ea448fde255af31c81f1a6c6e401392f3cbdbb8
markgrig <mark3012018@mail.ru>
2022-10-23 05:45:18 +0700

    23/10/22

--- pract/CLI/shop/functions/submission-created.js
+++ pract/CLI/shop/functions/submission-created.js
@@ -13,25 +13,8 @@ firebase.initializeApp(config);
 const db = firebase.database();
 
 exports.handler = async function(event, context, callback) {
-  const body = JSON.parse(event.body).payload
-  var newPostKey = db.ref().child(`submissions`).push().key;
-  db.ref(`submissions/${newPostKey}`).set({
-    body
-  }, function(error) {
-    if (error) {
-      console.log('failed')
-      return callback(null, {
-        statusCode: error.status,
-        body: JSON.stringify({
-          message: error.message,
-          error: error,
-        })
-      })
-    }
-    console.log('saved')
-    return callback(null, {
-      statusCode: 200,
-      body: "Beep, boop, you just got serverless."
-    })
-  })
+  return {
+    statusCode: 200,
+    body: JSON.stringify( db )
+  }
 }
