96c6212219e6c4f2aa7fd79a244ea2f4b5f90575
markgrig <mark3012018@mail.ru>
2022-10-23 06:32:10 +0700

    23/10/22

--- pract/CLI/shop/functions/HelloWorld2.js
+++ pract/CLI/shop/functions/HelloWorld2.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import { getDatabase } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -12,18 +12,19 @@ const firebaseConfig = {
   projectId: "markshop-d68ea",
   storageBucket: "markshop-d68ea.appspot.com",
   messagingSenderId: "338620385281",
-  appId: "1:338620385281:web:1b676faddf9906699bd800"
+  appId: "1:338620385281:web:1b676faddf9906699bd800",
+  databaseURL: "https://DATABASE_NAME.firebaseio.com",
 };
 
 // Initialize Firebase
-const appFire = initializeApp(firebaseConfig);
-const db = getFirestore(appFire);
+const appFirebase = initializeApp(firebaseConfig);
+const database = getDatabase(appFirebase);
 
 exports.handler = async function(event, context, callback) {
   return {
     statusCode: 200,
     body: JSON.stringify( {
-      message: db
+      message: database
     })
   }
 }
