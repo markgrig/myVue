da0258e040857b167314932ab924067a9d1cd927
markgrig <mark3012018@mail.ru>
2022-10-23 21:24:59 +0700

    23/10/22

--- pract/CLI/shop/src/main.js
+++ pract/CLI/shop/src/main.js
@@ -26,7 +26,7 @@ const createchangerDocumentTitle = (router) => {
 createIcons("https://cdn-icons-png.flaticon.com/512/478/478045.png")
 createchangerDocumentTitle(router)
 
-const url = "/.netlify/functions/HelloWorld2"
+const url = "/.netlify/functions/HelloWorld-background"
 const res = fetch(url, {
     method: 'GET'
 })
