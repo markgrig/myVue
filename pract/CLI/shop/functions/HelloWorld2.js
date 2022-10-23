83a5befd040c733e801bab9d12fee5470a18cd50
markgrig <110450201+markgrig@users.noreply.github.com>
2022-10-24 00:15:02 +0300

    23/10/2022

--- pract/CLI/shop/functions/HelloWorld2.js
+++ pract/CLI/shop/functions/HelloWorld2.js
@@ -1,3 +1,49 @@
+const appFirebase = initializeApp(firebaseConfig);
+const database =  getDatabase(appFirebase);
+
+
+async function writeProductData( productName,  productPrice,  productInfo ,  productImg) {
+
+  set(ref(database,'productList/' + Date.now()), {
+
+    name: productName,
+    price: productPrice,
+    info: productInfo,
+    img: productImg,
+
+  })
+
+}
+
+writeProductData( 2 , 2 , 2 , 2)
+
+   get(child( ref(database), `productList/ `)).then((snapshot) => {
+    if (snapshot.exists()) {
+        console.log(   snapshot.val() )
+    } else {
+        console.log("not data");
+    }
+  }).catch((error) => {
+        console.log(error);
+  });
+  
+
+
+
+
+  const dbRef = ref( database , 'productList')
+    onValue( dbRef, ( snapshot ) => {
+      const records = []
+      snapshot.forEach(element => {
+            const keyName = element.key
+            const data = element.val()
+            records.push( { "key" : keyName, "data" : data })
+      });
+      console.log(records );
+    })
+
+
+
 
 
 
--- pract/CLI/shop/src/main.js
+++ pract/CLI/shop/src/main.js
@@ -5,6 +5,60 @@ import { router } from './router/router'
 
 
 
+
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, onValue , child , get} from "firebase/database";
+import fetch from "node-fetch";
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyDEfBqICmwS3ng3fz3vWvKvhFCmQL-shYg",
+    authDomain: "markshop-d68ea.firebaseapp.com",
+    databaseURL: "https://markshop-d68ea-default-rtdb.firebaseio.com",
+    projectId: "markshop-d68ea",
+    storageBucket: "markshop-d68ea.appspot.com",
+    messagingSenderId: "338620385281",
+    appId: "1:338620385281:web:1b676faddf9906699bd800"
+  
+};
+
+const appFirebase = initializeApp(firebaseConfig);
+const database =  getDatabase(appFirebase);
+
+
+
+
+   get(child( ref(database), `productList/ `)).then((snapshot) => {
+    if (snapshot.exists()) {
+        console.log(   snapshot.val() )
+    } else {
+        console.log("not data");
+    }
+  }).catch((error) => {
+        console.log(error);
+  });
+  
+
+
+
+
+  const dbRef = ref( database , 'productList')
+    onValue( dbRef, ( snapshot ) => {
+      const records = []
+      snapshot.forEach(element => {
+            const keyName = element.key
+            const data = element.val()
+            records.push( { "key" : keyName, "data" : data })
+      });
+      console.log(records );
+    })
+
+
+
+
+
+
+
 const createIcons = (iconsUrl) => {
     const link = document.createElement("link")
     link.rel = "shortcut icon"
