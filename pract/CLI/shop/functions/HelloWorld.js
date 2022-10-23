559a895623e4050c69e034b716476ed28d502b40
markgrig <110450201+markgrig@users.noreply.github.com>
2022-10-24 01:27:55 +0300

    23/10/2022

--- pract/CLI/shop/functions/HelloWorld.js
+++ pract/CLI/shop/functions/HelloWorld.js
@@ -3,7 +3,20 @@ import { getDatabase } from "firebase/database";
 
 export const handler = async () => {
 
-  ;
+  const firebaseConfig = {
+
+    apiKey: process.env.API_KEY,
+    authDomain:  process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATA_BASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    
+  };
+  
+  const appFirebase = initializeApp(firebaseConfig);
+  const database =  getDatabase(appFirebase);
 
     return {
       statusCode: 200,
