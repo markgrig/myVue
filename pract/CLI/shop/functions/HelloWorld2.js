34835337cd9ce381e6f71278c3d352ca5e6c1e16
markgrig <mark3012018@mail.ru>
2022-10-23 09:10:41 +0700

    23/10/22

--- pract/CLI/shop/functions/HelloWorld2.js
+++ pract/CLI/shop/functions/HelloWorld2.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getDatabase, ref, set, onValue , child} from "firebase/database";
+import { getDatabase, ref, set, onValue , child , get} from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
