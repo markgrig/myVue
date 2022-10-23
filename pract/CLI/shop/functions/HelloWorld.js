b61dfe90de56d17b08cc0d2da0303171b514c9e4
markgrig <110450201+markgrig@users.noreply.github.com>
2022-10-23 22:47:05 +0300

    23/10/2022

--- pract/CLI/shop/functions/HelloWorld.js
+++ pract/CLI/shop/functions/HelloWorld.js
@@ -67,7 +67,7 @@ export const handler = async () => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message:  ref(database,`productList/`)
+      message:  child( ref(database,), `productList/`)
     })
   }
 
