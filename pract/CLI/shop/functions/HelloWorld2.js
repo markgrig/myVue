63c7ae7bf5c9b22eafa06f314f3ee88d0f19dc0b
markgrig <mark3012018@mail.ru>
2022-10-23 07:41:54 +0700

    23/10/22

--- pract/CLI/shop/functions/HelloWorld2.js
+++ pract/CLI/shop/functions/HelloWorld2.js
@@ -6,13 +6,13 @@ import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDEfBqICmwS3ng3fz3vWvKvhFCmQL-shYg",
-  authDomain: "markshop-d68ea.firebaseapp.com",
-  databaseURL: "https://markshop-d68ea-default-rtdb.firebaseio.com",
-  projectId: "markshop-d68ea",
-  storageBucket: "markshop-d68ea.appspot.com",
-  messagingSenderId: "338620385281",
-  appId: "1:338620385281:web:1b676faddf9906699bd800",
+  apiKey: process.env.API_KEY,
+  authDomain:  process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATA_BASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
 };
 
 // Initialize Firebase
