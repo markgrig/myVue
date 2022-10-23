e211cde9c4d39dc7262b5624059bcee97fc5755a
markgrig <110450201+markgrig@users.noreply.github.com>
2022-10-23 22:01:04 +0300

    23/10/2022

--- pract/CLI/shop/functions/HelloWorld.js
+++ pract/CLI/shop/functions/HelloWorld.js
@@ -34,7 +34,7 @@ const starCountRef = ref(database)
 
 exports.handler = function(event, context, callback) {
   
-  get(child( ref(database,`productList/` ))).then((snapshot) => {
+  get( ref(database,`productList/` )).then((snapshot) => {
     if (snapshot.exists()) {
       return {
           statusCode: 200,
